8893b0819a3c8269cdb94bfc234825d7d51d396a
Artemis-ion <1456721424@qq.com>
2021-01-13 06:22:49 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ npm install --registry=https://registry.npm.taobao.org/ && npm run build
 # 删除容器
 docker rm -f demo1 &> /dev/null
 
+sleep 5
+
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 18080:80 \
